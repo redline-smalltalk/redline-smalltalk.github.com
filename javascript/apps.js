0fe57d77b5d1996584ef69cf721d5b3b7716e477
Sean T Allen <sean@monkeysnatchbanana.com>
2011-10-20 09:27:55 -0400

    Cleans up formatting and removes duplicate image load.

--- javascript/apps.js
+++ javascript/apps.js
@@ -4,9 +4,9 @@
 
 images_to_preload = [
   'download-hover.png','documents-hover.png',
-	'docs-faq-hover.png','docs-thanks-hover.png','docs-icon-hover.png',
-	'download-icon-hover.png','discussion-hover.png','news-hover.png',
-	'twitter-hover.png','email-hover.png','community-hover.png','twitter-hover.png'
+  'docs-faq-hover.png','docs-thanks-hover.png','docs-icon-hover.png',
+  'download-icon-hover.png','discussion-hover.png','news-hover.png',
+  'twitter-hover.png','email-hover.png','community-hover.png'
 ];
 
 for (var i = 0; i < images_to_preload.length; i++) {
